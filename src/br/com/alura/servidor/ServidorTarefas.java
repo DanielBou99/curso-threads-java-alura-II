0fc3dfaeaf8ed4c766edb528f002612d47873767
Daniel <danielbou@outlook.com.br>
2022-08-19 11:38:09 -0300

    Design melhor, dado volatile e comandos em thread diferentes

--- src/br/com/alura/servidor/ServidorTarefas.java
+++ src/br/com/alura/servidor/ServidorTarefas.java
@@ -2,27 +2,48 @@ package br.com.alura.servidor;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ServidorTarefas {
+	
+	private ExecutorService threadPool;
+	private ServerSocket servidor;
+//	private volatile boolean estaRodando;
+	private AtomicBoolean estaRodando;
 
-	public static void main(String[] args) throws Exception {
-		
+	public ServidorTarefas() throws Exception {
 		System.out.println("#### Iniciando servidor ####");
-		ServerSocket servidor = new ServerSocket(12345);
-		
-//		ExecutorService threadPool = Executors.newFixedThreadPool(2);
-		ExecutorService threadPool = Executors.newCachedThreadPool();
-		
-		while(true) {
-			Socket accept = servidor.accept();
-			System.out.println("Aceitando novo cliente na porta " + accept.getPort());
-			
-			DistribuirTarefas distribuirTarefas = new DistribuirTarefas(accept);
-			threadPool.execute(distribuirTarefas);
+		this.servidor = new ServerSocket(12345);
+		this.threadPool = Executors.newFixedThreadPool(4);
+		this.estaRodando = new AtomicBoolean(true);
+	}
+
+	public void rodar() throws Exception {
+		while(estaRodando.get()) {
+			try {
+				Socket accept = servidor.accept();
+				System.out.println("Aceitando novo cliente na porta " + accept.getPort());
+				DistribuirTarefas distribuirTarefas = new DistribuirTarefas(threadPool , accept, this);
+				threadPool.execute(distribuirTarefas);
+			} catch (SocketException e) {
+				System.out.println("SocketException, está rodando? " + this.estaRodando);
+			}
 		}
-		
 	}
 	
+	public void parar() throws Exception {
+		estaRodando.set(false);
+		servidor.close();
+		threadPool.shutdown();
+	}
+	
+	public static void main(String[] args) throws Exception {
+		ServidorTarefas servidor = new ServidorTarefas();
+		servidor.rodar();
+		servidor.parar();
+	}
+
 }
