2bbb9594ab93758580cc108d0c588464696c5692
Daniel <danielbou@outlook.com.br>
2022-08-19 21:51:09 -0300

    BlockingQueue

--- src/br/com/alura/servidor/DistribuirTarefas.java
+++ src/br/com/alura/servidor/DistribuirTarefas.java
@@ -3,6 +3,7 @@ package br.com.alura.servidor;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 
@@ -11,9 +12,11 @@ public class DistribuirTarefas implements Runnable {
 	private Socket socket;
 	private ServidorTarefas servidor;
 	private ExecutorService threadPool;
+	private BlockingQueue<String> filaComandos;
 	
-	public DistribuirTarefas(ExecutorService threadPool, Socket socket, ServidorTarefas servidor) {
+	public DistribuirTarefas(ExecutorService threadPool, BlockingQueue<String> filaComandos, Socket socket, ServidorTarefas servidor) {
 		this.socket = socket;
+		this.filaComandos = filaComandos;
 		this.servidor = servidor;
 		this.threadPool = threadPool;
 	}
@@ -48,6 +51,11 @@ public class DistribuirTarefas implements Runnable {
 						this.threadPool.submit(new JuntaResultadosFutureWSFutureBanco(futureWS, futureBanco, saidaCliente));
 						break;
 					}
+					case "c3": {
+						this.filaComandos.put(comando);
+						saidaCliente.println("Comando c3 adicionado na fila");
+						break;
+					}
 					case "fim": {
 						saidaCliente.println("Desligando o servidor");
 						servidor.parar();
@@ -57,8 +65,7 @@ public class DistribuirTarefas implements Runnable {
 						saidaCliente.println("Outro comando: " + comando);
 					}
 				}
-
-				System.out.println(comando);
+//				System.out.println(comando);
 			}
 			
 			entradaCliente.close();
--- src/br/com/alura/servidor/ServidorTarefas.java
+++ src/br/com/alura/servidor/ServidorTarefas.java
@@ -3,9 +3,10 @@ package br.com.alura.servidor;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ServidorTarefas {
@@ -14,12 +15,24 @@ public class ServidorTarefas {
 	private ServerSocket servidor;
 //	private volatile boolean estaRodando;
 	private AtomicBoolean estaRodando;
+	private BlockingQueue<String> filaComandos;
 
 	public ServidorTarefas() throws Exception {
 		System.out.println("#### Iniciando servidor ####");
 		this.servidor = new ServerSocket(12345);
-		this.threadPool = Executors.newFixedThreadPool(4, new FabricaDeThread());
+		this.threadPool = Executors.newCachedThreadPool(new FabricaDeThread());
 		this.estaRodando = new AtomicBoolean(true);
+		this.filaComandos = new ArrayBlockingQueue<>(2);
+		iniciarConsumidores();
+	}
+
+	private void iniciarConsumidores() {
+		
+		int qtdConsumidores = 2;
+		for (int i = 0; i < qtdConsumidores; i++) {
+			TarefaConsumir tarefa = new TarefaConsumir(filaComandos);
+			this.threadPool.execute(tarefa);
+		}
 	}
 
 	public void rodar() throws Exception {
@@ -27,7 +40,7 @@ public class ServidorTarefas {
 			try {
 				Socket accept = servidor.accept();
 				System.out.println("Aceitando novo cliente na porta " + accept.getPort());
-				DistribuirTarefas distribuirTarefas = new DistribuirTarefas(threadPool , accept, this);
+				DistribuirTarefas distribuirTarefas = new DistribuirTarefas(threadPool, filaComandos, accept, this);
 				threadPool.execute(distribuirTarefas);
 			} catch (SocketException e) {
 				System.out.println("SocketException, está rodando? " + this.estaRodando);
